6fd48e81ffeb8f1464367dc1789c99d910f200b7
ccumgol <ccumgol@gmail.com>
2021-05-06 16:52:10 -0400

    counter.js 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = '리액트';
-  return <div className="react">{name}</div>;
-}
+const App = () => {
+  return (
+    <MyComponent name="React" favoriteNumber={3}>
+      리액트
+    </MyComponent>
+  );
+};
 
 export default App;
